111264250dae22bc08ca1e89bbdb065fbf448b70
Monhbayr05 <shmogii94@gmail.com>
2024-10-23 17:04:24 +0800

    products bas ip hayg sessiond hadgalah

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -23,7 +23,6 @@ class TableController extends Controller
 
     public function store(Request $request)
     {
-
         $validateData = $request->validate([
             'name'=>'required',
             'restaurant_id'=>'required',
@@ -33,7 +32,7 @@ class TableController extends Controller
 
         $incString = encrypt($randomString);
 
-        $content = 'http://13.115.248.34/QR/' . $incString;
+        $content = 'http://127.0.0.1:8000/products/' . $incString;
 
         $qr = QrCode::size(300)->margin(0)->generate($content);
 
@@ -51,16 +50,18 @@ class TableController extends Controller
 
     public function getTable($qr,Request $request)
     {
+        $products = Product::all();
 
         $ipAddress = $request->ip();
         session(['user_ip' => $ipAddress]);
+        dd($ipAddress);
 
         $qr = decrypt($qr);
         $table = Table::query()->where('qrcode', $qr)->first();
-        $products = Product::all();
 
 
-        return view('user.products', compact('table', 'products'));
+        return view('user.products',
+            compact('table', 'products'));
     }
 
     public function update(Request $request, $id)
--- resources/views/user/products.blade.php
+++
@@ -1,4 +0,0 @@
-@foreach($products as $product)
-    <h1>{{$product->name}}</h1>
-    <img src=" {{ asset($product->thumbnail) }} ">
-@endforeach
