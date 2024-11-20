d73fb2b5bde6ab21bfaed6032d6e347b25da0c7b
AnarTHEmegamind0 <82940783+AnarTHEmegamind0@users.noreply.github.com>
2024-11-20 16:53:11 +0800

    bas bus heden zuils

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,7 +21,7 @@
 
 
     <!-- bootstrap 4  -->
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendor/datatables/dataTables.bootstrap4.css')}}">
+    <!-- <link rel="stylesheet" href="{{ asset('admin/assets/vendor/datatables/dataTables.bootstrap4.css')}}"> -->
 
     <!-- bootstrap 5  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -63,6 +63,8 @@
 
 <!-- Page level plugins -->
 <!-- <script src="{{ asset('admin/assets/vendor/datatables/jquery.dataTables.min.js') }}"></script> -->
+
+<!-- eniig commentloson  -->
 <script src="{{ asset('admin/assets/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
 
@@ -74,10 +76,18 @@
 <!-- <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>  -->
 <!-- <script src="{{ asset('admin/assets/js/datatable/datatable_bundle.min.js') }}"></script> -->
 <!-- <script src="{{ asset('admin/assets/js/datatable/datatable_jquery.js') }}"></script> -->
-<script src="{{ asset('admin/assets/js/datatable/dataTables.bootstrap5.js') }}"></script>
+
+<!-- eniig bas  -->
+<!-- <script src="{{ asset('admin/assets/js/datatable/dataTables.bootstrap5.js') }}"></script>
 <script src="{{ asset('admin/assets/js/datatable/dataTables.js') }}"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script> -->
+
+
+<!-- bulma oruulj irsen -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bulma.min.js"></script>
 
 
 <!-- sweetalert  -->
--- resources/views/layouts/inc/chef/sidebar.blade.php
+++ resources/views/layouts/inc/chef/sidebar.blade.php
@@ -2,7 +2,7 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center"
-       href="{{route('admin.dashboard')}}">
+       href="{{route('chef.dashboard')}}">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
@@ -39,7 +39,6 @@
             <i class="fas fa-receipt"></i>
             <span>Захиалга</span></a>
     </li>
-    <!-- {{ route('chef.order.index') }} -->
 
 
 
