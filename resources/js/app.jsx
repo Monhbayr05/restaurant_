eb46b519c26a965c9ea10fb5306b147ea8da6f21
AnarTHEmegamind0 <82940783+AnarTHEmegamind0@users.noreply.github.com>
2025-01-09 15:47:16 +0800

    haha

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,15 +9,12 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `Foodbazalt`,
     resolve: (name) => 
         resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
         root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563', // Change this to your preferred color for the progress bar
     },
 });
 
