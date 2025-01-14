63bacc8193ae35e7551f3349f8e51b7f35c18589
AnarTHEmegamind0 <82940783+AnarTHEmegamind0@users.noreply.github.com>
2025-01-14 17:13:43 +0800

    ok

--- resources/js/Components/Cart.jsx
+++ resources/js/Components/Cart.jsx
@@ -33,23 +33,30 @@ const Cart = ({ cartItems, setCartItems }) => {
             setCartItems(updatedCart);
             localStorage.setItem("cart", JSON.stringify(updatedCart));
         } else {
-            const newCart = [...cartItems, { ...product, quantity: 1 }];
+            const newCart = [...cartItems, { ...product, quantity: 0 }];
             setCartItems(newCart);
             localStorage.setItem("cart", JSON.stringify(newCart));
         }
     };
 
     const handleRemoveFromCart = (product) => {
-        const updatedCart = cartItems
-            .map((item) =>
-                item.id === product.id && item.quantity > 1
+        const existingItem = cartItems.find((item) => item.id === product.id);
+
+        if (existingItem && existingItem.quantity > 1) {
+            const updatedCart = cartItems.map((item) =>
+                item.id === product.id
                     ? { ...item, quantity: item.quantity - 1 }
                     : item
-            )
-            .filter((item) => item.quantity > 0);
-
-        setCartItems(updatedCart);
-        localStorage.setItem("cart", JSON.stringify(updatedCart));
+            );
+            setCartItems(updatedCart);
+            localStorage.setItem("cart", JSON.stringify(updatedCart));
+        } else {
+            const updatedCart = cartItems.filter(
+                (item) => item.id !== product.id
+            );
+            setCartItems(updatedCart);
+            localStorage.setItem("cart", JSON.stringify(updatedCart));
+        }
     };
 
     const handleFormSubmit = (e) => {
