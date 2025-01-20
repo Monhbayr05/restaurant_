8fcb864cc4a674ec172e2b02b8708d86f33f0b3d
Monhbayr05 <shmogii94@gmail.com>
2025-01-20 11:25:11 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\User\OrderController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Route::controller(\App\Http\Controllers\User\OrderController::class)->group(function () {
+Route::controller(OrderController::class)->group(function () {
     Route::post('byl/webhook' , 'storeWebhook')->name('byl.webhook');
-
 });
-
-
-
-
-
